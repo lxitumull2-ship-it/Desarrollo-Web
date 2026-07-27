e3f128648a0ab66714c1a444c99c40ae9ef78d16
Lytzy Xitumul <lxitumull2@miumg.edu.gt>
2026-07-26 18:11:11 -0600

    Se corrigio en el index la db de departamentos

--- app/models/index.js
+++ app/models/index.js
@@ -39,8 +39,8 @@ db.sequelize = sequelize;
 // Le pasamos la instancia de conexión `sequelize` y la clase `Sequelize` como argumentos
 db.clientes = require("./cliente.model.js")(sequelize, Sequelize);
 db.productos = require("./productos.model.js")(sequelize, Sequelize);
-db.empleados = require("./empleados.model.js")(sequelize, Sequelize);
-db.departamentos = require("./departamentos.model.js")(sequelize, Sequelize);
+db.empleados = require("./empleado.model.js")(sequelize, Sequelize);
+db.departamentos = require("./departamento.model.js")(sequelize, Sequelize);
 // Aquí puedes seguir importando otros modelos de forma similar
 // Ejemplo: db.productos = require("./producto.model.js")(sequelize, Sequelize);
 
